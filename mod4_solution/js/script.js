6bab551966fd707d688f834d52b1bbcd2cf56d7c
hinescb96 <cedric.hines96@gmail.com>
2024-02-18 18:12:49 -0600

    Complete main part of assignment

--- mod4_solution/js/script.js
+++ mod4_solution/js/script.js
@@ -20,13 +20,6 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
-
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-
 */
 
 // STEP 1:
@@ -35,30 +28,50 @@ WARNING!!! WARNING!!!
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function() {
+  // STEP 10:
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
+  // See Lecture 50, part 1
+  for (var name in names) {
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+    // STEP 11:
+    // Retrieve the first letter of the current name in the loop.
+    // Use the string object's 'charAt' function. Since we are looking for
+    // names that start with either upper case or lower case 'J'/'j', call
+    // string object's 'toLowerCase' method on the result so we can compare
+    // to lower case character 'j' afterwards.
+    // Look up these methods on Mozilla Developer Network web site if needed.
+    var firstLetter = names[name].charAt(0).toLowerCase();
 
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
+    // STEP 12:
+    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop.
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[name])
+    } else {
+      helloSpeaker.speak(names[name])
+    }
   }
-}
+
+  // STEP 2 JHU SPECIFIC 
+  console.log("---Second List Print---")
+  function customMapFunction(value, index, array) {
+    var firstLetter = value.charAt(0).toLowerCase();
+    if (firstLetter === "j") {
+      return byeSpeaker.speakSimple(value);
+    } else {
+      return helloSpeaker.speakSimple(value);
+    }
+  }
+  var greetings = names.map(customMapFunction);
+  for (var greeting in greetings){
+    console.log(greetings[greeting]);
+  }
+
+  // STEP 3 JHU SPECIFIC (BONUS)
+  
+
+})();
