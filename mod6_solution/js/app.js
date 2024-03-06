07b1ba2bd1f65d8d9961df1fa284f816989ba0e1
hinescb96 <cedric.hines96@gmail.com>
2024-03-05 19:08:50 -0600

    Comments

--- mod6_solution/js/app.js
+++ mod6_solution/js/app.js
@@ -1,26 +1,36 @@
 (function() {
     'use-strict';
 
+    // Create Angular Module and add LunchCheckController
     angular.module('LunchCheck', [])
     .controller('LunchCheckController', LunchCheckController);
     
+    // Inject $scope to protect against minification
     LunchCheckController.$inject = ['$scope'];
+
+    // LunchCheckController Function
     function LunchCheckController ($scope) {
+        // lunchItemsString is bound to the input text box
         $scope.lunchItemsString = "";
 
+        // lunchItemsCheck() is bound to the button click event
         $scope.lunchItemsCheck = function() {
+
+            // Parse the input field text via CSV rules
             var numberOfItems = parseLunchItems($scope.lunchItemsString);
+
+            // Generate the message and the style class based off of the numberOfItems
             $scope.lunchStyleClass = generateLunchStyleClass(numberOfItems);
-            console.log($scope.lunchStyleClass);
             $scope.lunchMessage = generateLunchMessage(numberOfItems);
         };
     };
 
     function parseLunchItems(itemsString) {
+        // Split the input string via ','
         var items = itemsString.split(",");
 
+        // Count the number of items after splitting the input String
         var counter = 0;
-
         for (var i = 0; i < items.length; i++){
             if (items[i].trim().length > 0){
                 counter++;
@@ -35,6 +45,7 @@
             return "Please enter data first";
         }
 
+        // Generate Message based on number of values
         if (numberOfItems <= 3 ){
             return "Enjoy!";
         } else {
@@ -43,6 +54,7 @@
     }
 
     function generateLunchStyleClass(numberOfItems){
+        // Determine the style class to be used
         if (numberOfItems === 0){
             return "lunch-error";
         } else {
